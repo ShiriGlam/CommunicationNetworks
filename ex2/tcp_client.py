f92ae0da26ff72eb07d0c77c451a5e9c5a06720e
shiri glam <shiri3847@gmail.com>
2024-12-06 12:56:16 +0200

    origin

--- ex2/tcp_client.py
+++ ex2/tcp_client.py
@@ -1,7 +1,11 @@
 import socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(('127.0.0.1', 12345))
-s.send(b'hello')
+s.connect(('192.168.229.132', 12345))
+s.send(b'Shiri Glam')
 data = s.recv(100)
 print("Server sent: ", data)
+id_number = '213074628'
+s.send(id_number.encode())
+data_id = s.recv(100)
+print("Server sent: ", data_id)
 s.close()
