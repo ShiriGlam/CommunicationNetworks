3a05f595b1c90769a45f5302e8b1c02714fe06cb
shiri glam <shiri3847@gmail.com>
2024-12-07 18:14:34 +0200

    add v1....v4

--- ex2/tcp_client.py
+++ ex2/tcp_client.py
@@ -1,11 +1,21 @@
 import socket
+import time
+# יצירת חיבור
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(('192.168.229.132', 12345))
-s.send(b'Shiri Glam')
-data = s.recv(100)
-print("Server sent: ", data)
-id_number = '213074628'
+name = "Shiri Glam\n"
+s.send(name.encode())
+# קבלת תגובה רצופה לשתי ההודעות
+response1 = s.recv(512)# שליחת הודעה שנייה עם תו מפריד בסוף
+id_number = "213074628\n"
 s.send(id_number.encode())
-data_id = s.recv(100)
-print("Server sent: ", data_id)
+
+response2 = s.recv(512)
+
+time.sleep(2)
+# הדפסת תגובות נפרדות
+print("Server response for name: ", response1)
+print("Server response for ID number: ", response2)
+
+# סגירת חיבור
 s.close()
